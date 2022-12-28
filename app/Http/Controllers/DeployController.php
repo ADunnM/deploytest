3c178fe00e357869be9f95e29c61122f77b2acb6
ADunnM <anthony@metrixadvancedtechnologies.com>
2022-12-28 08:08:09 +0000

    a

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -13,7 +13,7 @@ class DeployController extends Controller
      if (hash_equals($githubHash, $localHash)) {
         echo '1';
           $root_path = base_path();
-          $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+          $process = new Process(['cd ' . $root_path . '; ./deploy.sh']);
           $process->run(function ($type, $buffer) {
               echo $buffer;
           });
