1fda5fa2d42879987f19306ca99dc5f15e0b93ea
ADunnM <anthony@metrixadvancedtechnologies.com>
2022-12-28 12:25:40 +0000

    asd

--- public/webhook.php
+++ public/webhook.php
@@ -11,11 +11,9 @@
         'echo $PWD',
         'whoami',
         'cd /var/www/root',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
+        'git pull origin main',
+        'test',
+
     );
 
     // exec commands
